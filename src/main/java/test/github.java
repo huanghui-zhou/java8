5e88d005711efbc97e523bb9f298cf67d8e8cbfa
huanghui-zhou <409924402@qq.com>
2018-09-19 20:19:07 +0800

    hello world

---
+++ src/main/java/test/github.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by huang on 2018/9/19.
+ */
+public class github {
+    public static void main(String[] str){
+        System.out.println("hello world github~~~");
+    }
+}
